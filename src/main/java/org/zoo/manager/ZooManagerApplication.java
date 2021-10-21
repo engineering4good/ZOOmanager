d356806053732ad2ef7b25f100f188efb99f637c
engineering4good <engineeringforgood@mail.ru>
2021-10-21 22:08:11 +0300

    initialized application

---
+++ src/main/java/org/zoo/manager/ZooManagerApplication.java
@@ -0,0 +1,21 @@
+package org.zoo.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class.
+ */
+@SpringBootApplication
+public class ZooManagerApplication {
+
+  /**
+   * Main method.
+   *
+   * @param args program arguments
+   */
+  public static void main(final String[] args) {
+    SpringApplication.run(ZooManagerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/zoo/manager/ZooManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zoo.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooManagerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
